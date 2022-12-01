6df50ca680240c3277c10aa925ef216e8b895cc4
Matt Perry <muffinman616@gmail.com>
2022-11-30 22:45:27 -0500

    Initial commit

---
+++ day1/day1.ts
@@ -0,0 +1,10 @@
+import { readInputLines } from '../shared/utils';
+
+const day1 = () => '';
+
+(async () => {
+    const input = await readInputLines('day1');
+
+    console.log(day1());
+    console.log(day1());
+})();
