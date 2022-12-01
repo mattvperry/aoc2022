5d20de9155b605091fd0891a55ab905e0a6ccb12
Matt Perry <forloop616@gmail.com>
2022-12-01 00:16:22 -0500

    day1

--- day1/day1.ts
+++ day1/day1.ts
@@ -1,10 +1,18 @@
-import { readInputLines } from '../shared/utils';
+import { map, readInputLines, splitOnEvery, sumBy } from '../shared/utils';
 
-const day1 = () => '';
+const parseInput = (lines: string[]): Iterable<number> => map(
+    splitOnEvery(lines, ""),
+    xs => sumBy(xs, x => parseInt(x, 10))
+);
+
+const day1 = (calories: Iterable<number>): number[] => Array
+    .from(calories)
+    .sort((a, b) => a < b ? 1 : -1);
 
 (async () => {
     const input = await readInputLines('day1');
-
-    console.log(day1());
-    console.log(day1());
+    const [a, b, c] = day1(parseInput(input));
+    
+    console.log(a);
+    console.log(a + b + c);
 })();
