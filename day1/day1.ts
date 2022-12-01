e129d09a9b9a4c0a32522693de4ef3a7e8acc19d
Matt Perry <forloop616@gmail.com>
2022-12-01 00:18:20 -0500

    Shring

--- day1/day1.ts
+++ day1/day1.ts
@@ -1,17 +1,19 @@
 import { map, readInputLines, splitOnEvery, sumBy } from '../shared/utils';
 
-const parseInput = (lines: string[]): Iterable<number> => map(
-    splitOnEvery(lines, ""),
-    xs => sumBy(xs, x => parseInt(x, 10))
-);
+const day1 = (lines: string[]): number[] => {
+    const calories = map(
+        splitOnEvery(lines, ""),
+        xs => sumBy(xs, x => parseInt(x, 10))
+    );
 
-const day1 = (calories: Iterable<number>): number[] => Array
-    .from(calories)
-    .sort((a, b) => a < b ? 1 : -1);
+    return Array
+        .from(calories)
+        .sort((a, b) => a < b ? 1 : -1);
+}
 
 (async () => {
     const input = await readInputLines('day1');
-    const [a, b, c] = day1(parseInput(input));
+    const [a, b, c] = day1(input);
     
     console.log(a);
     console.log(a + b + c);
